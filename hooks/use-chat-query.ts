efb9a9346eb5697b9009a146ec8c3873feefba97
Eamon Boyle <blaowskate@hotmail.com>
2023-10-04 00:12:41 +0100

    Remove socket

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -16,7 +16,7 @@ export const useChatQuery = ({
     paramKey,
     paramValue,
 }: ChatQueryProps) => {
-    const { isConnected } = useSocket();
+    // const { isConnected } = useSocket();
 
     const fetchMessages = async ({ pageParam = undefined }) => {
         const url = qs.stringifyUrl(
